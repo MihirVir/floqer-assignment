16381503613fd71959cc32782c5ada35a2a086be
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2024-05-21 00:11:16 +0100

    fixing vercel issue

--- client/src/pages/Analytics.tsx
+++ client/src/pages/Analytics.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "../components/header/header";
 import Table from "../components/table/Table";
 import { GroupByYearData, SecondTableData } from "@/types/TableProps";
